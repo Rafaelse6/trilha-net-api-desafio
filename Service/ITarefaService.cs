80db133fe3d9f850ba0c64cf13f05201f16adfba
Rafael Santos <rafa.05125@gmail.com>
2024-04-29 22:33:37 -0300

    Service

---
+++ Service/ITarefaService.cs
@@ -0,0 +1,16 @@
+﻿using TrilhaApiDesafio.Models;
+
+namespace TrilhaApiDesafio.Service
+{
+    public interface ITarefaService
+    {
+        Tarefa Create(Tarefa tarefa);
+        Tarefa FindById(int id);
+        List<Tarefa> FindAll();
+        Tarefa Update(Tarefa tarefa);
+        Tarefa FindByTitle(string title);
+        List<Tarefa> FindByDate(DateTime date);
+        List<Tarefa> FindByStatus(EnumStatusTarefa status);
+        void Delete(int id);
+    }
+}
---
+++ Service/Implementation/TarefaServiceImplementation.cs
@@ -0,0 +1,58 @@
+﻿using TrilhaApiDesafio.Models;
+using TrilhaApiDesafio.Repository.Generic;
+
+namespace TrilhaApiDesafio.Service.Implementation
+{
+    public class TarefaServiceImplementation : ITarefaService
+    {
+        private readonly IRepository<Tarefa> _repository;
+
+        public TarefaServiceImplementation(IRepository<Tarefa> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Tarefa> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Tarefa FindById(int id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Tarefa Create(Tarefa tarefa)
+        {
+            return _repository.Create(tarefa);
+        }
+
+        public Tarefa Update(Tarefa tarefa)
+        {
+            return _repository.Update(tarefa);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<Tarefa> FindByDate(DateTime date)
+        {
+            return _repository.FindAll().FindAll(t => t.Data.Date == date.Date);
+        }
+
+
+        public List<Tarefa> FindByStatus(EnumStatusTarefa status)
+        {
+            return _repository.FindAll().FindAll(t => t.Status == status);
+        }
+
+        public Tarefa FindByTitle(string title)
+        {
+            return _repository.FindAll().Find(t => t.Titulo == title);
+        }
+
+
+    }
+}
