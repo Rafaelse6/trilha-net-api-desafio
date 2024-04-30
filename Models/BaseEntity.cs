9dfd4818b5bfdb3372ab4271b3fd0befa7b69f91
Rafael Santos <rafa.05125@gmail.com>
2024-04-29 22:32:04 -0300

    BaseEntity

---
+++ Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrilhaApiDesafio.Models
+{
+    public class BaseEntity
+    {
+
+        [Column("id")]
+        public int Id { get; set; }
+    }
+}
